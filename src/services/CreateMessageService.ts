24d0901b84a7db33ab958405c1c196015f6dc072
devHikary <wrkkarina@gmail.com>
2021-10-20 20:55:27 -0300

    schema name null

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,5 +1,5 @@
 import prismaClient from "../prisma";
-
+import { io } from "../App";
 
 class CreateMessageService {
   async execute(text: string, user_id: string) {
@@ -13,8 +13,22 @@ class CreateMessageService {
       },
     });
 
+    const infoWS = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.created_at,
+      user: {
+        name: message.user.name,
+        avatar_url: message.user.avatar_url,
+      },
+    };
+
+    io.emit("new_message", infoWS);
+
     return message;
   }
+
 }
+
 
 export { CreateMessageService };
