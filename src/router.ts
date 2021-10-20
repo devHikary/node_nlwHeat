ff3d55f216f11f436826b985b310cfe0e07bd69b
devHikary <wrkkarina@gmail.com>
2021-10-20 20:41:38 -0300

    Criar profile do user 3 últimas mensagens

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { Get3LastMessagesController } from './controllers/GetLastMessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 
@@ -13,5 +15,9 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router}
